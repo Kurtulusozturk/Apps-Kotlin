45139479e349fa19c192b3a1f4e13f47fbe569ea
Kurtuluş Ruşen Öztürk <92689191+Kurtulusozturk@users.noreply.github.com>
2023-09-16 19:55:37 +0300

    ComposeIntro start

--- ComposeIntro/app/src/main/java/com/ozturkurtulus/composeintro/MainActivity.kt
+++ ComposeIntro/app/src/main/java/com/ozturkurtulus/composeintro/MainActivity.kt
@@ -3,41 +3,51 @@ package com.ozturkurtulus.composeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ozturkurtulus.composeintro.ui.theme.ComposeIntroTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeIntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(){
+    //Column, Row, Box
+    Column() {
+        Text(modifier = Modifier.background(color = Color.LightGray)
+            .padding(top = 10.dp)
+            .clickable {
+                println("tıkladın")
+            },
+            text = "Hello World!",
+            color = Color.Blue , fontSize = 25.sp,
+            fontWeight = FontWeight.ExtraBold)
+        Text(text = "Hello Android!",
+            color = Color.Blue,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.ExtraBold)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeIntroTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
